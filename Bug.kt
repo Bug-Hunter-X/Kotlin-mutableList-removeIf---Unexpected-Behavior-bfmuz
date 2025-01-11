e7708d930fed55f65942d0b80e83e740f99f2763
BugHunter-X <askzenai@gmail.com>
2025-01-11 12:10:05 +0100

    Add Bug.kt file

---
+++ Bug.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val list = mutableListOf(1, 2, 3, 4, 5)
+    list.removeIf { it > 2 }
+    println(list) // Output: [1, 2]
+    list.removeIf { it == 1 }
+    println(list) //Output: [2]
+    list.removeIf { it < 3 }
+    println(list) //Output: []
+}
